a4a06ca3e15f82f48b75a849a2cebe0b77ef1bce
Ashley Steigerwalt <asteiger@gmail.com>
2011-01-15 16:36:20 -0500

    Fix for localization

--- Classes/MetaCastAppDelegate.m
+++ Classes/MetaCastAppDelegate.m
@@ -16,20 +16,20 @@
 
 - (void)awakeFromNib {
 	statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength] retain]; 
-	[statusItem setTitle:@"Status"];
+	[statusItem setTitle:NSLocalizedString(@"MC", NULL)];
 	[statusItem setHighlightMode:YES];
 	
 	NSMenu *statusMenu = [[NSMenu allocWithZone:[NSMenu menuZone]] initWithTitle:@"MetaCast"];
 	
-	NSMenuItem *menuItem = [statusMenu addItemWithTitle:NSLocalizedString(@"BroadcastersMenuItem", nil) action:nil keyEquivalent:@""];
+	NSMenuItem *menuItem = [statusMenu addItemWithTitle:NSLocalizedString(@"Metacasters", NULL) action:nil keyEquivalent:@""];
 	[menuItem setSubmenu:[self metacastersMenu]];
 	
 	[statusItem setMenu:statusMenu];
 }
 
 - (NSMenu*)metacastersMenu {
-	NSMenu *metacastersMenu = [[NSMenu allocWithZone:[NSMenu menuZone]] initWithTitle:NSLocalizedString(@"BroadcastersMenuItem", nil)];
-	[metacastersMenu addItemWithTitle:NSLocalizedString(@"NoBroadcastersMenuItem", nil) action:nil keyEquivalent:@""];
+	NSMenu *metacastersMenu = [[NSMenu allocWithZone:[NSMenu menuZone]] initWithTitle:NSLocalizedString(@"Metacasters", NULL)];
+	[metacastersMenu addItemWithTitle:NSLocalizedString(@"No Metacasters Found", NULL) action:nil keyEquivalent:@""];
 
 	return metacastersMenu;
 }
